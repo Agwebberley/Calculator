d33f4de147a481c9ffb26328af8a828b4426747d
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-04-22 09:14:17 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 root = Tk()
 root.title("Calculator")
@@ -24,7 +25,7 @@ def button_addF():
 	button = "add"
 	first_number = e.get()
 	global f_num 
-	f_num = int(first_number)
+	f_num = int(float(first_number))
 	e.delete(0, END)
 
 def button_subtractF():
@@ -32,7 +33,7 @@ def button_subtractF():
 	button = "subtract"
 	first_number = e.get()
 	global f_num
-	f_num = int(first_number)
+	f_num = int(float(first_number))
 	e.delete(0, END)
 
 
@@ -41,7 +42,7 @@ def button_multiplyF():
 	button = "multiply"
 	first_number = e.get()
 	global f_num
-	f_num = int(first_number)
+	f_num = int(float(first_number))
 	e.delete(0, END)
 
 def button_divideF():
@@ -49,7 +50,7 @@ def button_divideF():
 	button = "divide"
 	first_number = e.get()
 	global f_num
-	f_num = int(first_number)
+	f_num = int(float(first_number))
 	e.delete(0, END)
 
 
@@ -57,40 +58,65 @@ def button_equalF():
 	if button == "add":
 		second_number = e.get()
 		e.delete(0, END)
-		e.insert(0, int(f_num) + int(second_number))
+		e.insert(0, int(float(f_num)) + int(float(second_number)))
 	elif button == "subtract":
 		second_number = e.get()
 		e.delete(0, END)
-		e.insert(0, int(f_num) - int(second_number))
+		e.insert(0, int(float(f_num)) - int(float(second_number)))
 	elif button == "multiply":
 		second_number = e.get()
 		e.delete(0, END)
-		e.insert(0, int(f_num) * int(second_number))
+		e.insert(0, int(float(f_num)) * int(float(second_number)))
 	elif button == "divide":
 		second_number = e.get()
 		e.delete(0, END)
-		e.insert(0, int(f_num) / int(second_number))
+		e.insert(0, int(float(f_num)) / int(float(second_number)))
+
+def button_sqF():
+	num = e.get()
+	num = int(float(num))
+	new_num = math.sqrt(num)
+	e.delete(0, END)
+	e.insert(0, new_num)
+
+def button_sF():
+	num = e.get()
+	num = int(float(num))
+	new_num = num*num
+	e.delete(0, END)
+	e.insert(0, new_num)
 
 
 #Define buttons
-Button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-Button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-Button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-Button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-Button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-Button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-Button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-Button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-Button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-Button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+frame19 = LabelFrame(root, padx=5, pady=5)
+frame19.grid(row=1, column=0)
 
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_equalF)
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clearF)
+framebottom = LabelFrame(root, padx=5, pady=5)
+framebottom.grid(row=2, column=0)
 
-button_add = Button(root, text="+", padx=39, pady=20, command=button_addF)
-button_subtract = Button(root, text="-", padx=39, pady=20, command=button_subtractF)
-button_multiply = Button(root, text="*", padx=39, pady=20, command=button_multiplyF)
-button_divide = Button(root, text="/", padx=39, pady=20, command=button_divideF)
+frameside = LabelFrame(root, padx=5, pady=5)
+frameside.grid(row=1, rowspan=4, column=1)
+
+Button_1 = Button(frame19, text="1", padx=40, pady=20, command=lambda: button_click(1))
+Button_2 = Button(frame19, text="2", padx=40, pady=20, command=lambda: button_click(2))
+Button_3 = Button(frame19, text="3", padx=40, pady=20, command=lambda: button_click(3))
+Button_4 = Button(frame19, text="4", padx=40, pady=20, command=lambda: button_click(4))
+Button_5 = Button(frame19, text="5", padx=40, pady=20, command=lambda: button_click(5))
+Button_6 = Button(frame19, text="6", padx=40, pady=20, command=lambda: button_click(6))
+Button_7 = Button(frame19, text="7", padx=40, pady=20, command=lambda: button_click(7))
+Button_8 = Button(frame19, text="8", padx=40, pady=20, command=lambda: button_click(8))
+Button_9 = Button(frame19, text="9", padx=40, pady=20, command=lambda: button_click(9))
+Button_0 = Button(framebottom, text="0", padx=40, pady=20, command=lambda: button_click(0))
+
+button_equal = Button(framebottom, text="=", padx=91, pady=20, command=button_equalF)
+button_clear = Button(framebottom, text="Clear", padx=79, pady=20, command=button_clearF)
+
+button_add = Button(frameside, text="+", padx=39, pady=20, command=button_addF)
+button_subtract = Button(frameside, text="-", padx=39, pady=20, command=button_subtractF)
+button_multiply = Button(frameside, text="*", padx=39, pady=20, command=button_multiplyF)
+button_divide = Button(frameside, text="/", padx=39, pady=20, command=button_divideF)
+button_s = Button(frameside, text="x²", padx=39, pady=20, command=button_sF)
+button_sq = Button(frameside, text="√", padx=39, pady=20, command=button_sqF)
 
 #put buttons on the screen
 Button_7.grid(row=1, column=0)
@@ -108,11 +134,13 @@ Button_3.grid(row=3, column=2)
 Button_0.grid(row=4, column=0)
 
 button_clear.grid(row=4, column=1, columnspan=2,)
-button_add.grid(row=5, column=0)
+button_add.grid(row=4, column=0)
 button_equal.grid(row=5,column=1, columnspan=2,)
 
-button_subtract.grid(row=1, column=4)
-button_multiply.grid(row=2, column=4)
-button_divide.grid(row=3, column=4)
+button_subtract.grid(row=1, column=0)
+button_multiply.grid(row=2, column=0)
+button_divide.grid(row=3, column=0)
+button_sq.grid(row=6, column=0)
+button_s.grid(row=5, column=0)
 
 root.mainloop()
